6cdadebee73d4424e1a24b0ca6d4231d98fed7a5
mbwa-md <fundimbwa@gmail.com>
2026-02-24 13:31:35 +0300

    sila

---
+++ config.js
@@ -0,0 +1,27 @@
+// config.js
+
+const config = {
+  BOT_NAME: 'SILA MD',
+  OWNER_NAME: 'SILA TECH',
+  OWNER_NUMBER: '255612491554',
+  BOT_VERSION: '2.0.0',
+  BOT_FOOTER: '*𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚒𝚕𝚊 𝚃𝚎𝚌𝚑*',
+  BOT_URL: 'https://sila-free-bot-2026.onrender.com',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://sila_md:sila0022@sila.67mxtd7.mongodb.net/',
+  NEWSLETTER_JIDS: ['120363402325089913@newsletter', '120363421404091643@newsletter'],
+  RCD_IMAGE_PATH: 'https://files.catbox.moe/277zt9.jpg',
+  BOT_IMAGES: [
+    'https://files.catbox.moe/277zt9.jpg',
+    'https://files.catbox.moe/277zt9.jpg'
+  ],
+  OWNER_NUMBERS: ['255789661031', '255612491554'],
+  AUTO_JOIN_LINKS: [
+    'https://whatsapp.com/channel/0029VbBPxQTJUM2WCZLB6j28',
+    'https://whatsapp.com/channel/0029VbBG4gfISTkCpKxyMH02',
+    'https://whatsapp.com/channel/0029VbBmFT430LKO7Ch9C80X',
+    'https://chat.whatsapp.com/IdGNaKt80DEBqirc2ek4ks',
+    'https://chat.whatsapp.com/C03aOCLQeRUH821jWqRPC6'
+  ]
+};
+
+module.exports = config;
